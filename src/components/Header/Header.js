0de4abef6432f66f977ec8a3448923f5ba886bff
Joe Fogg <GJF610@gmail.com>
2018-01-16 19:46:00 -0500

    YES

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => (
+
+<h1> Hi I'm a Header. </h1> 
+ 	);
+
+
+export default Header;
