41b5e182016c7674b8cfd04bcd343dc4db1a29b4
James <jwinham4740@gmail.com>
2018-01-22 15:19:39 -0500

    FLORIDA MARKERS SHOWING

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import logo from './tag.jpg';
 import "./Header.css";
 
--- src/components/Omap/Omap.js
+++ src/components/Omap/Omap.js
@@ -1,32 +1,54 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Omap.css";
+import React from "react"
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-const Omap = () => (
-
-        <omap className = "Omap-header" >
-        <h2 className = "Omap-title" > WORLD ORDERS < /h2>
-        <div className = "row"> </div>
-        <div className = "col-md-12" > WORLD ORDERS </div>
-
-
-
-
-
-
-
-
-
-    </omap>
-
-
-
-
-
-
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAlQxc9ffXAI1u7GffGKDFSeo6b6HOIDPE&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} onClick={props.onMarkerClick} />}
+    {props.isMarkerShown && <Marker position={{ lat: 27.9506, lng: -82.4572 }} onClick={props.onMarkerClick} />}
+  </GoogleMap>
 );
 
+class MyFancyComponent extends React.PureComponent {
+  state = {
+    isMarkerShown: false,
+  }
 
+  componentDidMount() {
+    this.delayedShowMarker()
+  }
 
-export default Omap;
+  delayedShowMarker = () => {
+    setTimeout(() => {
+      this.setState({ isMarkerShown: true })
+    }, 3000)
+  }
 
+  handleMarkerClick = () => {
+    this.setState({ isMarkerShown: false })
+    this.delayedShowMarker()
+  }
+
+  render() {
+    return (
+      <MyMapComponent
+        isMarkerShown={this.state.isMarkerShown}
+        onMarkerClick={this.handleMarkerClick}
+      />
+    )
+  }
+}
+
+export default MyFancyComponent;
